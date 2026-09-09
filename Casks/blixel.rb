7851498140a909e7783e101bcf59dcf0a07e1cb2
Lucas Lima <lucas.lsl.lima@gmail.com>
2026-09-09 14:04:50 -0300

    blixel 0.6.0

--- Casks/blixel.rb
+++ Casks/blixel.rb
@@ -1,6 +1,6 @@
 cask "blixel" do
-  version "0.5.3"
-  sha256 "e7d1257208934c852304b56da7f0600154075965ed54d51179f627b4a4cfe386"
+  version "0.6.0"
+  sha256 "45daeec6c8a88c320356238b3991afa10d9d972d1276f0dca413e7bfc058ee0f"
 
   url "https://github.com/blixel-app/blixel-releases/releases/download/v#{version}/Blixel-#{version}.dmg"
   name "Blixel"
@@ -11,6 +11,8 @@ cask "blixel" do
     url :url
     strategy :github_latest
   end
+
+  auto_updates true
 
   depends_on macos: :sequoia
   depends_on arch: :arm64
