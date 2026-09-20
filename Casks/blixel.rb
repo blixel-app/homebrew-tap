bc3eb00104303f418face16d7d531a9b834cd7b1
Lucas Lima <lucas.lsl.lima@gmail.com>
2026-09-20 19:14:16 -0300

    blixel 0.7.0

--- Casks/blixel.rb
+++ Casks/blixel.rb
@@ -1,6 +1,6 @@
 cask "blixel" do
-  version "0.6.3"
-  sha256 "e61f5a4bff17125fb07cfe8e6076744cfc41e9c96562b9965a5a13bbdb48365c"
+  version "0.7.0"
+  sha256 "c39d7baddf0f8d0e04305488b9a89e27a41bce7f96886ee904527111c898f95e"
 
   url "https://github.com/blixel-app/blixel-releases/releases/download/v#{version}/Blixel-#{version}.dmg"
   name "Blixel"
